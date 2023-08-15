cb1d296add71f3a41f31914b48d4b875d71a8895
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2023-08-15 19:22:07 +0300

    now you can draw , added eventListeners

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
     //this will create 16 DIVs 
     const mainContainer = document.querySelector('.mainContainer');
     for(let i=0;i<16;i++){
-        const div = document.createElement('div');
+        let div = document.createElement('div');
         div.classList.add('square');
-      mainContainer.appendChild(div);
+        div.addEventListener('mouseenter',()=>div.style.backgroundColor='black');
+        mainContainer.appendChild(div);
         for(let j=0;j<15;j++){
             const div = document.createElement('div');
             div.classList.add('square');
+            div.addEventListener('mouseenter',()=>div.style.backgroundColor='black');
             mainContainer.appendChild(div);
-
         }
     }
+
         
