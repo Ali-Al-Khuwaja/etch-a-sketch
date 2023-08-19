76a4df2c013df7f36640c80628cd988046e76bde
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2023-08-19 12:00:17 +0300

    add the ability to change the size of the grid

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-    //this will create 16 DIVs 
-    const mainContainer = document.querySelector('.mainContainer');
-    for(let i=0;i<16;i++){
+//this will create 16 DIVs 
+const container = document.querySelector('.container');
+createNewGrid(16);
+
+function createNewGrid(value){
+    const howManySquares = value ; //get the number of total width and height
+    const squareSizePercentage = 100 / value; 
+    console.log(squareSizePercentage);
+    container.innerHTML = ''; //clear old grid by assigning it to nothing
+    for(let i=0;i<howManySquares;i++){
         let div = document.createElement('div');
         div.classList.add('square');
         div.addEventListener('mouseenter',()=>div.style.backgroundColor='black');
-        mainContainer.appendChild(div);
-        for(let j=0;j<15;j++){
+        div.style.width = `${squareSizePercentage}%`; // Set square width
+        div.style.height = `${squareSizePercentage}%`; // Set square height
+        container.appendChild(div);
+        for(let j=0;j<howManySquares-1;j++){
             let div = document.createElement('div');
             div.classList.add('square');
             div.addEventListener('mouseenter',()=>div.style.backgroundColor='black');
-            mainContainer.appendChild(div);
+            div.style.width = `${squareSizePercentage}%`; // Set square width
+            
+            div.style.height = `${squareSizePercentage}%`; // Set square height
+            container.appendChild(div);
         }
     }
+}
 
-        
+function newGrid(){
+    let value = prompt('what is the size of your desired grid ?');
+    return createNewGrid(value);
+
+}
+
