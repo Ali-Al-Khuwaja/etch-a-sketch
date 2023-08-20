51ae8b7519f723312b9a84978d5417d413dad7c8
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2023-08-20 11:57:05 +0300

    add basic ui and clear button

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
+let currentGridSize = '';
 //this will create 16 DIVs 
-const container = document.querySelector('.container');
-createNewGrid(16);
+const gridContainer = document.querySelector('.gridContainer');
+newGrid(16);
 
 function createNewGrid(value){
     const howManySquares = value ; //get the number of total width and height
     const squareSizePercentage = 100 / value; 
     console.log(squareSizePercentage);
-    container.innerHTML = ''; //clear old grid by assigning it to nothing
+    gridContainer.innerHTML = ''; //clear old grid by assigning it to nothing
     for(let i=0;i<howManySquares;i++){
         let div = document.createElement('div');
         div.classList.add('square');
         div.addEventListener('mouseenter',()=>div.style.backgroundColor='black');
         div.style.width = `${squareSizePercentage}%`; // Set square width
         div.style.height = `${squareSizePercentage}%`; // Set square height
-        container.appendChild(div);
+        gridContainer.appendChild(div);
         for(let j=0;j<howManySquares-1;j++){
             let div = document.createElement('div');
             div.classList.add('square');
@@ -21,14 +22,24 @@ function createNewGrid(value){
             div.style.width = `${squareSizePercentage}%`; // Set square width
             
             div.style.height = `${squareSizePercentage}%`; // Set square height
-            container.appendChild(div);
+            gridContainer.appendChild(div);
         }
     }
 }
 
-function newGrid(){
-    let value = prompt('what is the size of your desired grid ?');
-    return createNewGrid(value);
+
+function newGrid(autoValue){
+    if(autoValue===undefined){
+        let value = prompt('what is the size of your desired grid ?');
+        currentGridSize = value;
+        console.log(currentGridSize);
+        return createNewGrid(value);
+    }
+    currentGridSize = autoValue;
+    return createNewGrid(autoValue);
 
 }
-
+function clearGrid(){
+    gridContainer.textContent='';
+    createNewGrid(currentGridSize);
+}
